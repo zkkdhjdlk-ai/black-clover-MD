ee5199ab30067b5286f8187d35a6e7e229159edc
Carlos <thecarlospcok@gmail.com>
2025-04-24 01:49:03 -0600

    Add files via upload

---
+++ plugins/ComprarBot.js
@@ -0,0 +1,15 @@
+const handler = async (m, {conn}) => {
+  m.reply(global.ComprarBot);
+};
+handler.command ='comprarbot',/^(ComprarBot|Comprar|comprar|ComprarBot)$/i;
+export default handler;
+
+global.ComprarBot = `
+〔 *BLACK CLOVER- BOT* 〕
+
+*BOT PARA GRUPO* :
+> wa.me/527971006556
+
+*BOT PERZONALIZADO* :
+> wa.me/525544876071
+`;
