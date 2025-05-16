77d4de43648a47cebd3d0d66bb3874a1b361e659
Carlos <thecarlospcok@gmail.com>
2025-05-15 19:43:07 -0600

    Update info-estado.js

--- plugins/info-estado.js
+++ plugins/info-estado.js
@@ -3,7 +3,7 @@ let handler = async (m, { conn, usedPrefix, isRowner}) => {
 let _muptime
 let totalreg = Object.keys(global.db.data.users).length
 let totalchats = Object.keys(global.db.data.chats).length
-let pp = "https://qu.ax/uoRRA.jpg"
+let pp = "https://files.catbox.moe/8lfoj3.jpg"
 if (process.send) {
 process.send('uptime')
 _muptime = await new Promise(resolve => {
