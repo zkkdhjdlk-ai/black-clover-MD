59e7600837ceee34dbce1cfeced6b85ceb80f378
zkkdhjdlk-ai <zkkdhjdlk@gmail.com>
2025-08-27 12:49:58 +0100

    Update info-creator.js

--- plugins/info-creator.js
+++ plugins/info-creator.js
@@ -1,7 +1,11 @@
 import fetch from 'node-fetch'
 
 let handler = async (m, { conn }) => {
-  let txt_owner = "`𝙷𝙾𝙻𝙰, 𝙴𝚂𝚃𝙴 𝙴𝚂 𝙴𝙻 𝙽𝚄𝙼𝙴𝚁𝙾 𝙳𝙴 𝙼𝙸 𝙲𝚁𝙴𝙰𝙳𝙾𝚁, 𝙲𝚄𝙰𝙻𝚀𝚄𝙸𝙴𝚁 𝙵𝙰𝙻𝙻𝙰 𝙾 𝚂𝙸 𝚀𝚄𝙸𝙴𝚁𝙴𝚂 𝙰𝙶𝚁𝙴𝙶𝙰𝚁 𝙴𝙻 𝙱𝙾𝚃 𝙰 𝚃𝚄 𝙶𝚁𝚄𝙿𝙾, 𝙿𝚄𝙴𝙳𝙴𝚂 𝙷𝙰𝙱𝙻𝙰𝚁𝙻𝙴`\n\n  𝐓𝐇𝐄 𝐂𝐀𝐑𝐋𝐎𝐒: +525544876071"
+  let txt_owner = "`
+
+*"مرحبًا، هذا هو رقم منشئي. لأي مشكلة أو إذا كنت تريد إضافة البوت إلى مجموعتك، يمكنك التواصل معه."*
+
+*𝐓𝐇𝐄 𝐂𝐀𝐑𝐋𝐎𝐒:*`\n\n  𝐓𝐇𝐄 𝐂𝐀𝐑𝐋𝐎𝐒: +212680821981"
   try {
     let res = await fetch("https://files.catbox.moe/l1ahc0.jpg")
     let buffer = await res.buffer()
